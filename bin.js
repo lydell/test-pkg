1483e4e7f7ccf6032bace7ddd831d071d462c6ad
Simon Lydell <simon.lydell@gmail.com>
2021-01-11 07:28:16 +0100

    elm

--- bin.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-console.log("bin.js", __dirname);
-
-const eslint = require(require.resolve("eslint", {paths: [process.cwd(), ...require.resolve.paths("eslint")]}));
-
-console.log("eslint", eslint);
